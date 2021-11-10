b4226f550a14117ce1e1180e661e5fbb62b06921
dahall <dahall@byu.net>
2021-11-10 11:02:13 -0700

    Moved supporting files to SharedFiles folder

--- src/SharedFiles/Settings.Designer.cs
+++ src/SharedFiles/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace IntentionalSolutionVersion.Properties {
+namespace Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
